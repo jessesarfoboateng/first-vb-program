703b330db82c1a0a86fca4262b4e2505c8993350
Jesse <boatengjesse921@gmail.com>
2025-06-27 03:07:03 +0000

    Add VB project files with .gitignore

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        MsgBox("hello world")
+        MsgBox("How are you")
+        MsgBox("I hope you are well")
+
+
+
+    End Sub
+End Class
